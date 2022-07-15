a8f415ddf1454d393df3abce6f625c23dd91e4b1
Liubzzz <cosmin.liuba67@gmail.com>
2022-07-15 14:21:38 +0300

    text bar and add button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -50,6 +50,15 @@ watch(checkbox, newValue => {
 <template>
     <span>Two-way data binding (v-model):</span>
     <input type="checkbox" v-model="checkbox" />
+    <div>
+        <span><input type="text"/></span>
+         <button class="button-33" role="button">Add</button>
+    
+    
+    
+    </div>
+   
+
     <p>Updated: {{ counter.updated }}</p>
     <p>Deleted: {{ counter.deleted }}</p>
     <div class="todo-list">
@@ -70,4 +79,35 @@ watch(checkbox, newValue => {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
 }
+
+
+
+
+
+
+/* CSS */
+.button-33 {
+  background-color: #c2fbd7;
+  border-radius: 100px;
+  box-shadow: rgba(44, 187, 99, .2) 0 -25px 18px -14px inset,rgba(44, 187, 99, .15) 0 1px 2px,rgba(44, 187, 99, .15) 0 2px 4px,rgba(44, 187, 99, .15) 0 4px 8px,rgba(44, 187, 99, .15) 0 8px 16px,rgba(44, 187, 99, .15) 0 16px 32px;
+  color: green;
+  cursor: pointer;
+  display: inline-block;
+  font-family: CerebriSans-Regular,-apple-system,system-ui,Roboto,sans-serif;
+  padding: 7px 20px;
+  text-align: center;
+  text-decoration: none;
+  transition: all 250ms;
+  border: 0;
+  font-size: 16px;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-33:hover {
+  box-shadow: rgba(44,187,99,.35) 0 -25px 18px -14px inset,rgba(44,187,99,.25) 0 1px 2px,rgba(44,187,99,.25) 0 2px 4px,rgba(44,187,99,.25) 0 4px 8px,rgba(44,187,99,.25) 0 8px 16px,rgba(44,187,99,.25) 0 16px 32px;
+  transform: scale(1.05) rotate(-1deg);
+}
+
 </style>
